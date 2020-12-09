a5a63286647b73e9f604e371a7f4b0df51669df3
Jeff Bennett <jab6ft@virginia.edu>
2020-12-08 21:29:11 -0500

    Added sensitivity_comparison.py to analyze the sensitivity at several locations within the BCT

---
+++ projects/mid_atlantic/expected_case_n/plot_Fig5_geophysical_parameters.py
@@ -0,0 +1,172 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.patches as mpatches
+
+# =====================================
+# user inputs
+# =====================================
+# data input
+results_filename = "uncertainty_results_all.csv"
+savename = "Fig5_geophysical_parameters.png"
+
+# figure resolution
+DPI = 400  # Set resolution for saving figures
+
+x_var = "permeability_mD"
+x_label = "Permeability (mD)"
+x_convert = 1.0
+x_limit = []
+x_scale = 'log'
+
+y_vars = ["porosity", "thickness_m", ]
+y_labels = ["Porosity (-)", "Thickness (m)"]
+y_converts = [1.0, 1.0]
+y_limits = [[], []]
+y_scales = ['linear', 'log']
+
+series_var = 'RTE'
+series_convert = 100.0
+series_label = "Efficiency (%)"
+
+formation_dict = {'LK1': 'Lower Cretaceous', 'MK1-3': 'Middle Cretaceous', 'UJ1': 'Upper Jurassic'}
+
+markersize = 2
+# =====================================
+# process data
+# =====================================
+
+# Import results
+df = pd.read_csv(results_filename)
+
+# Fill empty values as zero
+df = df.fillna(0.0)
+
+# Set efficiency to 0 for all errors
+# df.loc[df.errors == False, 'RTE'] = 0.0
+
+# Not interested in cases with thickness less than 5 m
+# df = df.loc[df.thickness_m > 5.0]
+
+# =====================================
+# create plots
+# =====================================
+
+# Column width guidelines https://www.elsevier.com/authors/author-schemas/artwork-and-media-instructions/artwork-sizing
+# Single column: 90mm = 3.54 in
+# 1.5 column: 140 mm = 5.51 in
+# 2 column: 190 mm = 7.48 i
+width = 10.0  # inches
+height = 6.5  # inches
+
+# Create plot
+f, a = plt.subplots(len(y_vars), len(df.sheet_name.unique()), sharex=True, sharey='row', squeeze=False,
+                    constrained_layout=True)
+
+# Set size
+f.set_size_inches(width, height)
+
+# Set style and context
+sns.set_style("white", {"font.family": "serif", "font.serif": ["Times", "Palatino", "serif"]})
+sns.set_context("paper")
+sns.set_style("ticks", {"xtick.major.size": 8, "ytick.major.size": 8})
+
+# color map
+cmap = 'RdYlGn'
+
+# access min and max RTE for equal colobars
+vmin = 100 * df.loc[:, 'RTE'].min()
+vmax = 100 * df.loc[:, 'RTE'].max()
+
+count = 0
+# iterate through y-variables
+for j, (y_var, y_label, y_convert, y_limit, y_scale) in enumerate(
+        zip(y_vars, y_labels, y_converts, y_limits, y_scales)):
+
+    # iterate through x-variables
+    for i, (formation) in enumerate(df.sheet_name.unique()):
+
+        # access subplot
+        ax = a[j, i]
+
+        # get data and convert
+        df2 = df[(df.loc[:, 'sheet_name'] == formation)]
+        x = x_convert * df2.loc[:, x_var]
+        y = y_convert * df2.loc[:, y_var]
+        c = 100 * df2.loc[:, 'RTE']
+
+        # plot
+        im = ax.scatter(x, y, c=c, s=markersize, marker='.', cmap=cmap, vmin=vmin, vmax=vmax)
+
+        # plot successful combinations
+        # df2 = df[(df.loc[:, 'RTE'] > 0.4) & (df.loc[:, 'sheet_name'] == formation)]
+        # x = x_convert * df2.loc[:, x_var]
+        # y = y_convert * df2.loc[:, y_var]
+        # ax.scatter(x, y, c='black', s=markersize, marker='.')
+
+        # plot failures
+        # df2 = df[(df.loc[:, 'RTE'] <= 0.4) & (df.loc[:, 'sheet_name'] == formation)]
+        # x = x_convert * df2.loc[:, x_var]
+        # y = y_convert * df2.loc[:, y_var]
+        # ax.scatter(x, y, c='red', s=markersize, marker='.')
+
+        # axes scales
+        ax.set_xscale(x_scale)
+        ax.set_yscale(y_scale)
+
+        # axes labels
+        # x-axis labels (only bottom)
+        if j == len(y_vars) - 1:
+            ax.set_xlabel(x_label)
+        else:
+            ax.get_xaxis().set_visible(False)
+
+        # y-axis labels (only left side)
+        if i == 0:
+            ax.set_ylabel(y_label)
+        else:
+            ax.get_yaxis().set_visible(False)
+
+        # Despine and remove ticks
+        sns.despine(ax=ax, )
+        ax.tick_params(top=False, right=False)
+
+        # Axes limits
+        if len(y_limit) == 2:
+            ax.set_ylim(bottom=y_limit[0], top=y_limit[1])
+
+        # top labels
+        if j == 0:
+            plt.text(0.5, 1.1, formation_dict[formation], horizontalalignment='center', verticalalignment='center',
+                     transform=ax.transAxes, fontsize='medium', fontweight='bold')
+
+        # Caption labels
+        caption_labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O']
+        plt.text(0.1, 0.9, caption_labels[count], horizontalalignment='center', verticalalignment='center',
+                 transform=ax.transAxes, fontsize='medium', fontweight='bold')
+        count = count + 1
+
+# Legend
+# patches = [mpatches.Patch(color='black', label='RTE > 40%'), mpatches.Patch(color='red', label='RTE <= 40%')]
+# a[1, 1].legend(handles=patches, bbox_to_anchor=(0.5, -0.275), loc="upper center", ncol=2)
+a_cbar = []
+for ax in a:
+    a_cbar.append(ax[-1])
+cbar = f.colorbar(im, ax=a_cbar, location='right', shrink=0.6)
+# cbar = f.colorbar(im, ax=a[1, 1], orientation='horizontal', pad=0.2)
+cbar.ax.set_title('RTE [%]')
+# cbar.set_label('RTE [%]', rotation=0)
+# Adjust layout
+# plt.tight_layout()
+plt.subplots_adjust(top=0.848,
+                    bottom=0.13,
+                    left=0.125,
+                    right=0.85,
+                    hspace=0.2,
+                    wspace=0.2)
+f.align_ylabels(a[:, 0])  # align y labels
+
+# Save Figure
+# plt.savefig(savename, dpi=DPI, bbox_extra_artists=leg)
+plt.savefig(savename, dpi=DPI)
+# plt.close()
---
+++ projects/mid_atlantic/expected_case_n/plot_FigSI1_ISO_location_results.py
@@ -0,0 +1,89 @@
+# General imports
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import matplotlib.lines as mlines
+
+# Set Color Palette
+colors = sns.color_palette("colorblind")
+# Set resolution for saving figures
+DPI = 300
+
+# Column width guidelines https://www.elsevier.com/authors/author-schemas/artwork-and-media-instructions/artwork-sizing
+# Single column: 90mm = 3.54 in
+# 1.5 column: 140 mm = 5.51 in
+# 2 column: 190 mm = 7.48 i
+width = 7.48  # inches
+height = 5.5  # inches
+
+# %%=============================================================================#
+# Figure 6 - LCOE
+results_filename = "uncertainty_results_all.csv"
+savename = "FigSI1_ISO_location_results.png"
+# =============================================================================#
+y_vars = ["RTE", "duration", "kW_out_avg"]
+y_labels = ["RTE\n[%]", "Duration\n(hr)", "Power\n(MW)"]
+y_converts = [100.0, 1.0, 1e-3]
+y_limits = [[], [], []]
+
+pal = 'colorblind'
+
+df_raw = pd.read_csv(results_filename)
+
+# select locations of interest and provide case to label
+cases = ['PJM', ' New England ISO', 'ISO NY']
+sheet_names = ['LK1', 'LK1', 'LK1']
+xs = [-49904.42, 385475.58, 147995.58]
+ys = [4108774.11, 4504574.11, 4464994.11]
+
+df = pd.DataFrame()
+for case, sheet_name, x, y in zip(cases, sheet_names, xs, ys):
+    df_slice = df_raw[(df_raw.loc[:, 'sheet_name'] == sheet_name) &
+                      (df_raw.loc[:, 'X (m)'] == x) &
+                      (df_raw.loc[:, 'Y (m)'] == y)]
+    if len(df_slice) > 0:
+        df_slice.loc[:, 'Analysis Case'] = case
+        df = df.append(df_slice)
+
+# calculate additional
+df.loc[:, 'duration'] = df.loc[:, 'kWh_out'] / df.loc[:, 'kW_out_avg']
+# filla in nan
+df = df.fillna(0.0)
+
+# save location results
+df.to_csv('ISO_location_results.csv')
+
+f, a = plt.subplots(1, 3, sharey=True)
+a = a.ravel()
+
+# Set size
+f.set_size_inches(width, height)
+legend = [False, False, False]
+for ax, y_var, y_label, y_convert, y_limit, leg in zip(a, y_vars, y_labels, y_converts, y_limits, legend):
+    df.loc[:, y_var] = df.loc[:, y_var] * y_convert
+    sns.histplot(data=df, x=y_var, ax=ax, hue='Analysis Case', multiple='dodge', element='step', fill=False, legend=leg,
+                 palette=pal,)
+    ax.set_xlabel(y_label)
+    # sns.distplot(df[y_var], ax=ax)
+
+# a[1].legend(bbox_to_anchor=(0.5, -0.15), ncol=3)
+
+lines = []
+for case, color in zip(cases, sns.color_palette(pal)):
+    lines.append(mlines.Line2D([], [], color=color, linestyle='-', marker='None', markersize=9,
+                               markerfacecolor='None', markeredgewidth=1.5,
+                               label=case))
+a[1].legend(handles=lines, bbox_to_anchor=(0.5, -0.15), loc="upper center", title='Location', ncol=3)
+
+plt.subplots_adjust(top=0.88, bottom=0.23, left=0.1, right=0.9, hspace=0.205, wspace=0.2)
+
+# Save Figure
+plt.savefig(savename, dpi=DPI)
+# plt.close()
+
+df = df.fillna(0.0)
+for case in df.loc[:, 'Analysis Case'].unique():
+    ind = (df.loc[:, 'Analysis Case'] == case) & (df.loc[:, 'RTE'] == 0.0)
+    print(case)
+    print(str(sum(ind)))
