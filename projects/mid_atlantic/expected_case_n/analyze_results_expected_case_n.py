9800bd753cb1de061f3d9fcaa9487dec6948f671
Jeff Bennett <jab6ft@virginia.edu>
2021-01-14 23:29:06 -0500

    Updated plotting

--- projects/mid_atlantic/expected_case_n/analyze_results_expected_case_n.py
+++ projects/mid_atlantic/expected_case_n/analyze_results_expected_case_n.py
@@ -6,12 +6,15 @@ import pandas as pd
 results_file = 'uncertainty_results_all.csv'
 formations = ['LK1', 'MK1-3', 'UJ1']  # needs to be present in the sheet_name column of the results_file
 ocean_data_files = ['LK1_ocean_data.xls', 'MK1_3_ocean_data.xls', 'UJ1_ocean_data.xls']
+sizing_file = 'study_results.csv'
+
 
 # ------------------- #
 # begin program
 # ------------------- #
 # read-in results
 df_results = pd.read_csv(results_file)
+df_sizing = pd.read_csv(sizing_file)
 
 # fill in nan values
 df_results = df_results.fillna(0.0)
@@ -32,9 +35,11 @@ for formation, ocean_data_file in zip(formations, ocean_data_files):
         X_m = df_ocean.loc[i, 'X_m'].values[0]
         Y_m = df_ocean.loc[i, 'Y_m'].values[0]
 
-        # select relevant entries in df_results
+        # select relevant entries in df_results and sizing
         ind = (df_results.loc[:, 'sheet_name'] == formation) & (df_results.loc[:, 'X (m)'] == X_m) & (
                     df_results.loc[:, 'Y (m)'] == Y_m)
+        ind2 = (df_sizing.loc[:, 'sheet_name'] == formation) & (df_sizing.loc[:, 'X (m)'] == X_m) & (
+                    df_sizing.loc[:, 'Y (m)'] == Y_m)
 
         # store formation name
         df_ocean.loc[i, 'formation'] = formation
@@ -60,6 +65,10 @@ for formation, ocean_data_file in zip(formations, ocean_data_files):
         n_fail = sum(df_results.loc[ind, 'RTE'] == 0.0)
         df_ocean.loc[i, 'feasible_fr'] = 1.0 - n_fail / n_entries
         df_ocean.loc[i, 'infeasible_fr'] = n_fail / n_entries
+
+        # Sizing
+        df_ocean.loc[i, 'm_dot'] = df_sizing.loc[ind2, 'm_dot']
+        df_ocean.loc[i, 'r_f'] = df_sizing.loc[ind2, 'r_f']
 
     # save results to new csv
     savename = formation + '_analysis.csv'
